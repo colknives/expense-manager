fe7480c6f703ac445d455a1b57e465a8f11e4492
Coleen Kier Ambrosio <coleen.ambrosio@playup.com>
2020-05-23 19:50:22 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::redirect('/', '/login');
+
+Route::get('/login', ['as' => 'auth.login.show', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('/login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@login']);
+
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
+    Route::post('/logout', ['as' => 'login', 'uses' => 'Auth\LoginController@logout']);
+
+});
+
+
